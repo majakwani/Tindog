2967578aebe936f565b4540d532f07496ba4d74b
majakwani <anasjakwani72@gmail.com>
2023-01-18 18:28:08 +0500

    Data of dogs used and imported from another js files

---
+++ data.js
@@ -0,0 +1,28 @@
+const dogsData = [
+    {
+        name: "Rex",
+        imgPath: "./images/dog-rex.jpg",
+        age: 25,
+        bio: "Art. Literature. Natural wine. Yoga.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },{
+        name: "Bella",
+        imgPath: "./images/dog-bella.jpg",
+        age: 43,
+        bio: "Yup, that's my owner. U can meet him if you want",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Teddy",
+        imgPath: "./images/dog-teddy.jpg",
+        age: 30,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    }
+]
+
+export default dogsData
+
